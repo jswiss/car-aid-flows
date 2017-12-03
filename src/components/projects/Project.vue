76c2c11e40d6f4fe61749a02c5ba856e45bbbb4d
Joshua Swiss <swissjoshua@gmail.com>
2017-12-03 13:00:34 +0300

    first mvp of project page donezo

--- src/components/projects/Project.vue
+++ src/components/projects/Project.vue
@@ -11,169 +11,138 @@
       </div>
       <div class="info-box">
         <div class="info-title">
-          <h3 class="title is-3">Basic Information</h3>
+          <h3 class="title is-3">Information sommaire</h3>
         </div>
         <div id="basic-info" class="info-box">
           <div id="dates" class="columns is-mobile">
             <div class="column is-5" id="start-date">
-              <h4 class="title is-4 is-bold">Start Date:</h4>
-              <p class="date-text">{{ project['Start Date'] || 'n/a' }}</p>
+              <h4 class="title is-4 is-bold">Date de début:</h4>
+              <p class="date-text">{{ project['Date de début'] || 'n/a' }}</p>
             </div>
             <div class="column is-2"></div>
             <div class="column is-5" id="end-date">
-              <h4 class="title is-4">End Date:</h4>
-              <p class="date-text">{{ project['End Date'] }}</p>
+              <h4 class="title is-4">Date de clôture:</h4>
+              <p class="date-text">{{ project['Date de clôture'] }}</p>
             </div>
           </div>
           <div id="dates" class="columns is-mobile">
-            <div class="column is-4">
-              <h4 class="title is-4 is-bold"><span class="ndp">NDP Pillar:</span></h4>
-              <p class="pillar">{{ project['NDP Pillar'] }}</p>
+            <div class="column is-3">
+              <h4 class="title is-5 is-bold"><span class="ndp">Composante du RCPCA:</span></h4>
+              <p class="pillar">{{ project['Composante'] }}</p>
             </div>
-            <div class="column is-4">
-              <h4 class="title is-4 is-bold"><span class="ndp">Primary Sector:</span></h4>
-              <p class="pillar">{{ project['Primary Sector'] }}</p>
+            <div class="column is-3">
+              <h4 class="title is-5 is-bold"><span class="ndp">Pilier du RCPCA:</span></h4>
+              <p class="pillar">{{ project['Pilier'] }}</p>
             </div>
-            <div class="column is-4">
-              <h4 class="title is-4 is-bold"><span class="ndp">Related Sectors:</span></h4>
-              <p class="pillar">{{ project['Links to other Sector'] || 'n/a' }}</p>
+            <div class="column is-3">
+              <h4 class="title is-5 is-bold"><span class="ndp">Secteur principal:</span></h4>
+              <p class="pillar">{{ project['Secteur principal'] }}</p>
+            </div>
+            <div class="column is-3">
+              <h4 class="title is-5 is-bold"><span class="ndp">Liens vers d'autres secteurs:</span></h4>
+              <p class="pillar">{{ project["Liens vers d'autres secteurs"] || 'n/a' }}</p>
             </div>
           </div>
           <div id="desc" class="columns is-mobile">
             <div class="column is-5" id="description">
-              <h4 class="title is-4 is-bold">Description:</h4>
-              <p class="desc-text">{{ project['Project objectives / purpose'] || 'n/a' }}</p>
+              <h4 class="title is-4 is-bold">Objectifs du projet:</h4>
+              <p class="desc-text">{{ project['Objectifs du projet'] || 'n/a' }}</p>
             </div>
             <div class="column is-2"></div>
             <div class="column is-4" id="project-website">
-              <h4 class="title is-4">Project Website:</h4>
-              <a v-bind:href="project['Links to project webpages or documents']"><p class="url">{{ project['Links to project webpages or documents'] || 'n/a'}}</p></a>
+              <h4 class="title is-4">Site web du projet:</h4>
+              <a v-bind:href="project['Liens vers site internet ou documents du projet']"><p class="url">{{ project['Liens vers site internet ou documents du projet'] || 'n/a'}}</p></a>
             </div>
           </div>
         </div>
         <div class="info-title">
-          <h3 class="title is-3">Financial Information</h3>
+          <h3 class="title is-3">L'information financière</h3>
         </div>
         <div id="financial-info" class="info-box">
           <div id="value" class="columns is-mobile">
             <div class="column is-3" id="project-value">
-              <h4 class="title is-4 is-bold">Total Project Value:</h4>
-              <p id="total-value" class="desc-text">${{ project['Project Value (USD)'] | currency }}</p>
+              <h4 class="title is-5 is-bold"><strong>Montant du projet (XAF)</strong>:</h4>
+              <p id="total-value" class="desc-text">FCFA {{ project['Montant du projet XAF'] | currency }}</p>
+              <p id="total-value-small" class="desc-text-small">€ {{ project['Montant du projet XAF'] / xafToEur | currency }}</p>
+              <p id="total-value-small" class="desc-text-small">$ {{ project['Montant du projet XAF'] / usd | currency }}</p>
             </div>
             <div class="column is-3">
-              <h4 class="title is-4 is-bold"><span class="disb">2015 Disbursement:</span></h4>
-              <p class="year-disb">${{ project['2015 Disbursements (USD)'] | currency }}</p>
+              <h4 class="title is-6 is-bold"><span class="disb">Décaissements 2017:</span></h4>
+              <p class="year-disb">FCFA {{ project['Décaissements 2017 XAF'] | currency }}</p>
+              <p id="total-value-small" class="desc-text-small">€ {{ project['Décaissements 2017 XAF'] / xafToEur | currency }}</p>
+              <p id="total-value-small" class="desc-text-small">$ {{ project['Décaissements 2017 XAF'] / usd | currency }}</p>
             </div>
             <div class="column is-3">
-              <h4 class="title is-4 is-bold"><span class="disb">2016 Disbursement:</span></h4>
-              <p class="year-disb">${{ project['2016 Disbursements (USD)'] | currency }}</p>
+              <h4 class="title is-6 is-bold"><span class="disb">Prévision de décaissements 2018:</span></h4>
+              <p class="year-disb">FCFA {{ project['Prévision de décaissements 2018 XAF'] + project['Prévision de décaissements 2017 XAF'] | currency }}</p>
+              <p id="total-value-small" class="desc-text-small">€ {{ project['Décaissements 2018 XAF'] / xafToEur | currency }}</p>
+              <p id="total-value-small" class="desc-text-small">$ {{ project['Décaissements 2018 XAF'] / usd | currency }}</p>
             </div>
             <div class="column is-3">
-              <h4 class="title is-4 is-bold"><span class="disb">2017 Disbursement:</span></h4>
-              <p class="year-disb">${{ project['2017 Disbursements (USD)'] | currency }}</p>
+              <h4 class="title is-6 is-bold"><span class="disb">Prévision de décaissements 2019:</span></h4>
+              <p class="year-disb">FCFA {{ project['Prévision de décaissements 2019 XAF'] | currency }}</p>
+              <p id="total-value-small" class="desc-text-small">€ {{ project['Décaissements 2019 XAF'] / xafToEur | currency }}</p>
+              <p id="total-value-small" class="desc-text-small">$ {{ project['Décaissements 2019 XAF'] / usd | currency }}</p>
             </div>
-          </div>
-          <div id="table-allocation">
-            <h3 class="title is-4 is-bold">Project Disbursements by Year & Location</h3>
-            <table class="table is-bordered is-striped is-narrow">
-            <thead>
-              <tr>
-                <th><abbr title=""></abbr></th>
-                <th><abbr title="Federal Government of Somalia">FGS</abbr></th>
-                <th><abbr title="Benadir">Benadir</abbr></th>
-                <th><abbr title="2015, USD">Galmudug</abbr></th>
-                <th><abbr title="Hiiraan & Middle Shabelle">HiirShabelle</abbr></th>
-                <th><abbr title="Jubaland">Jubaland</abbr></th>
-                <th><abbr title="Puntland">Puntland</abbr></th>
-                <th><abbr title="Somaliland">Somaliland</abbr></th>
-                <th><abbr title="South West">South West</abbr></th>
-                <th><abbr title="Unattributed">Unattributed</abbr></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="year-row">2015</td>
-                <td>${{ project['FGS - 2015'] || 0 | currency }}</td>
-                <td>${{ project['Benadir - 2015'] || 0 | currency }}</td>
-                <td>${{ project['Galmudug - 2015'] || 0 | currency }}</td>
-                <td>${{ project['Hiiraan & Middle Shabelle - 2015'] || 0 | currency }}</td>
-                <td>${{ project['Jubaland - 2015'] || 0 | currency }}</td>
-                <td>${{ project['Puntland - 2015'] || 0 | currency }}</td>
-                <td>${{ project['Somaliland - 2015'] || 0 | currency }}</td>
-                <td>${{ project['South West - 2015'] || 0 | currency }}</td>
-                <td>${{ project['Unattributed - 2015'] || 0 | currency }}</td>
-              </tr>
-              <tr>
-                <td class="year-row">2016</td>
-                <td>${{ project['FGS - 2016'] || 0 | currency }}</td>
-                <td>${{ project['Benadir - 2016'] || 0 | currency }}</td>
-                <td>${{ project['Galmudug - 2016'] || 0 | currency }}</td>
-                <td>${{ project['Hiiraan & Middle Shabelle - 2016'] || 0 | currency }}</td>
-                <td>${{ project['Jubaland - 2016'] || 0 | currency }}</td>
-                <td>${{ project['Puntland - 2016'] || 0 | currency }}</td>
-                <td>${{ project['Somaliland - 2016'] || 0 | currency }}</td>
-                <td>${{ project['South West - 2016'] || 0 | currency }}</td>
-                <td>${{ project['Unattributed - 2016'] || 0 | currency }}</td>
-              </tr>
-              <tr>
-                <td class="year-row">2017</td>
-                <td>${{ project['FGS - 2017'] || 0 | currency }}</td>
-                <td>${{ project['Benadir - 2017'] || 0 | currency }}</td>
-                <td>${{ project['Galmudug - 2017'] || 0 | currency }}</td>
-                <td>${{ project['Hiiraan & Middle Shabelle - 2017'] || 0 | currency }}</td>
-                <td>${{ project['Jubaland - 2017'] || 0 | currency }}</td>
-                <td>${{ project['Puntland - 2017'] || 0 | currency }}</td>
-                <td>${{ project['Somaliland - 2017'] || 0 | currency }}</td>
-                <td>${{ project['South West - 2017'] || 0 | currency }}</td>
-                <td>${{ project['Unattributed - 2017'] || 0 | currency }}</td>
-              </tr>
-            </tbody>
-          </table>
           </div>
         </div>
         <div class="info-title">
-          <h3 class="title is-3">Funders and Implementers</h3>
+          <h3 class="title is-3">Bailleurs de fonds et exécutants</h3>
         </div>
         <div id="basic-info" class="info-box">
           <div id="funder" class="columns is-mobile">
             <div class="column is-5">
-              <h4 class="title is-4 is-bold"><span class="donor">Reporting Agency:</span></h4>
-              <p class="funder">{{ project['Reporting Agency'] || 'n/a' }}</p>
+              <h4 class="title is-4 is-bold"><span class="donor">Agence de reportage:</span></h4>
+              <p class="funder">{{ project['Agence de reportage'] || 'n/a' }}</p>
             </div>
             <div class="column is-2"></div>
             <div class="column is-5">
-              <h4 class="title is-4 is-bold"><span class="imp">Implementer(s):</span></h4>
-              <p class="implementer" id="implementer">{{ project['Implementers'] || 'n/a' }}</p>
+              <h4 class="title is-4 is-bold"><span class="imp">Partenaires d'exécution:</span></h4>
+              <p class="implementer" id="implementer">{{ project["Partenaires d'exécution"] || 'n/a' }}</p>
             </div>
           </div>
           <div id="implementer" class="columns is-mobile">
             <div class="column is-5">
-              <h4 class="title is-4 is-bold"><span class="donor">Funder(s):</span></h4>
-              <p class="funder">{{ project['Funders'] || 'n/a' }}</p>
+              <h4 class="title is-4 is-bold"><span class="donor">Bailleurs:</span></h4>
+              <p class="funder">{{ project['Bailleurs'] || 'n/a' }}</p>
             </div>
             <div class="column is-2"></div>
             <div class="column is-5">
-              <h4 class="title is-4 is-bold"><span class="imp">Implementer Category:</span></h4>
-              <p class="implementer" id="imp-category">{{ project['Implementer Category'] || 'n/a' }}</p>
+              <h4 class="title is-4 is-bold"><span class="imp">Ministère de tutelle:</span></h4>
+              <p class="implementer" id="imp-category">{{ project['Ministère de tutelle'] || 'n/a' }}</p>
             </div>
           </div>
         </div>
         <div class="info-title">
-          <h3 class="title is-3">Project Thematic Focuses</h3>
+          <h3 class="title is-3">Themes transversaux</h3>
         </div>
         <div id="basic-info" class="info-box">
           <div id="focuses" class="columns is-mobile">
             <div class="column is-4">
-              <h4 class="title is-4 is-bold"><span class="focus">Gender:</span></h4>
-              <p class="proj-focus">{{ project.Gender || 'n/a' }}</p>
+              <h4 class="title is-4 is-bold"><span class="focus">Atténuer les déséquilibres régionaux:</span></h4>
+              <p class="proj-focus">{{ project['Atténuer les déséquilibres régionaux'] || 'n/a' }}</p>
             </div>
             <div class="column is-4">
-              <h4 class="title is-4 is-bold"><span class="focus">Capacity Building:</span></h4>
-              <p class="proj-focus">{{ project['Capacity building'] || 'n/a' }}</p>
+              <h4 class="title is-4 is-bold"><span class="focus">Promouvoir l'égalité des genres:</span></h4>
+              <p class="proj-focus">{{ project["Promouvoir l'égalité des genres"] || 'n/a' }}</p>
             </div>
             <div class="column is-4">
-              <h4 class="title is-4 is-bold"><span class="focus">Stabilization:</span></h4>
-              <p class="proj-focus">{{ project.Stabilization || 'n/a' }}</p>
+              <h4 class="title is-4 is-bold"><span class="focus">Renforcer la transparence et la redevabilité:</span></h4>
+              <p class="proj-focus">{{ project['Renforcer la transparence et la redevabilité'] || 'n/a' }}</p>
+            </div>
+          </div>
+          <div id="focuses" class="columns is-mobile">
+            <div class="column is-4">
+              <h4 class="title is-4 is-bold"><span class="focus">Accroître les capacités nationales:</span></h4>
+              <p class="proj-focus">{{ project['Accroître les capacités nationales'] || 'n/a' }}</p>
+            </div>
+            <div class="column is-4">
+              <h4 class="title is-4 is-bold"><span class="focus">Promouvoir l'inclusion des jeunes:</span></h4>
+              <p class="proj-focus">{{ project["Promouvoir l'inclusion des jeunes"] || 'n/a' }}</p>
+            </div>
+            <div class="column is-4">
+              <h4 class="title is-4 is-bold"><span class="focus">Environnement et exploitation durable des ressources:</span></h4>
+              <p class="proj-focus">{{ project['Environnement et exploitation durable des ressources'] || 'n/a' }}</p>
             </div>
           </div>
         </div>
@@ -183,7 +152,9 @@
 </template>
 
 <script type="text/javascript">
+  import moment from 'moment';
   import store from '../../store';
+  import { excelToJsDate } from '../../utils/helpers';
   const projects = store.state.rawData;
 
   export default {
@@ -192,13 +163,10 @@
   		return {
   			projects: projects,
   			project: null,
+  			eur: this.$store.state.eur,
+  			usd: this.$store.state.usd,
+  			xafToEur: this.$store.state.xafToEur,
   		};
-  	},
-  	mounted() {
-  		this.fetchData();
-  	},
-  	watch: {
-  		$route: 'fetchData',
   	},
   	methods: {
   		fetchData() {
@@ -212,6 +180,30 @@
   		printPage(el) {
   			window.print();
   		},
+  		dateFormat() {
+  			this.projects.forEach(d => {
+  				if (d['Date de début'] !== null) {
+  					d['Date de début'] = moment(excelToJsDate(d['Date de début'])).format(
+  						'MMM, YYYY'
+  					);
+  				}
+  				if (d['Date de clôture'] !== null) {
+  					d['Date de clôture'] = moment(
+  						excelToJsDate(d['Date de clôture'])
+  					).format('MMM, YYYY');
+  				}
+  			});
+  		},
+  	},
+  	mounted() {
+  		this.dateFormat();
+  		this.fetchData();
+  	},
+  	watch: {
+  		$route: 'fetchData',
+  	},
+  	beforeCreate() {
+  		this.dateFormat();
   	},
   };
 </script>
@@ -243,6 +235,10 @@
   }
   #total-value {
   	font-weight: bold;
+  }
+  #total-value-small,
+  .pillar {
+  	font-size: 13px;
   }
   #value {
   	margin-top: 1%;
