869b6e6464a6129139dc1b05880b54273d2e4a55
Joshua Swiss <swissjoshua@gmail.com>
2017-12-05 14:34:19 +0300

    basic drilldown working, back button not, sadly

--- src/components/charts/PillarCountChart.vue
+++ src/components/charts/PillarCountChart.vue
@@ -3,31 +3,65 @@
   <div>
     <div class="columns is-mobile">
       <div class="column" id="container"></div>
+			<a id="back" @click="backButton" :class="{display: isDisplay}" class="button is-pulled-right">Retour aux piliers</a>
     </div>
   </div>
 </template>
 <script type="text/javascript">
 	export default {
 		name: 'pillarCountChart',
+		data() {
+			return {
+				isDisplay: true,
+			};
+		},
 		mounted() {
 			this.pillarChart();
 		},
 		methods: {
+			backButton() {
+				this.isDisplay = !this.isDisplay;
+				chart.getSeries(0).data(this.$store.state.pillarCountChart);
+			},
 			pillarChart() {
-				const chart = anychart.column();
+				const vm = this;
+				const chart = anychart.column(this.$store.state.pillarCountChart);
 				// chart.animate(true);
 				chart.title('Nombre de projets par piliers et composantes');
-				const series = chart.column(this.$store.state.pillarCountChart);
-				// set series tooltip settings
-				series.tooltip().titleFormat('{%X}');
+				chart.tooltip().format('{%value} projets');
+				// tune interactivity selection mode
+				chart.interactivity().selectionMode('none');
 
-				series
-					.tooltip()
-					.position('center-top')
-					.anchor('center-bottom')
-					.offsetX(0)
-					.offsetY(5)
-					.format('${%Value}{groupsSeparator: }');
+				chart.label(0, {
+					enabled: false,
+					position: 'rightTop',
+					anchor: 'rightTop',
+					padding: 5,
+					offsetX: 5,
+					offsetY: 5,
+					text: 'Back',
+					background: { stroke: '1 black', enabled: true },
+				});
+
+				// load initial data on label click
+				chart.label(0).listen('click', function() {
+					chart.getSeries(0).data(data);
+					chart.label(0).enabled(false);
+				});
+
+				chart.listen('pointClick', function(e) {
+					// check if there is drillDown data available
+					if (e.point.get('drillDown')) {
+						// if so, assign to the only data series we have
+						chart.getSeries(0).data(e.point.get('drillDown'));
+						vm.backButton();
+					} else {
+						// otherwise assign this series the initial
+						// dataset and return to the initial state
+						chart.getSeries(0).data(this.$store.state.pillarCountChart);
+						vm.backButton();
+					}
+				});
 
 				// set scale minimum
 				chart.yScale().minimum(0);
@@ -43,8 +77,8 @@
 				chart.interactivity().hoverMode('by-x');
 
 				// axes titles
-				chart.xAxis().title('Product');
-				chart.yAxis().title('Revenue');
+				// chart.xAxis().title('Nombre de projets');
+				chart.yAxis().title('Nombre de projets');
 
 				// set container id for the chart
 				chart.container('container');
@@ -70,5 +104,19 @@
 		font-style: italic;
 		font-size: 12px;
 		color: gray;
+	}
+	#back {
+		cursor: pointer;
+		background: #7bc0f7;
+		border: 1px solid #64b5f6;
+		color: #fff;
+		padding: 10px;
+		position: absolute;
+		top: 15px;
+		right: 20px;
+		z-index: 100;
+	}
+	.display {
+		display: none;
 	}
 </style>
