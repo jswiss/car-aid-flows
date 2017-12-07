70002df54e48f30a23a28c53d01b9eaf2eb25df5
Joshua Swiss <swissjoshua@gmail.com>
2017-12-07 16:47:50 +0300

    added french text to home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,24 @@
 <!--home page-->
 <template>
   <div id="topmost" class="columns is-gapless is-mobile">  
-    <div class="column is-3">
-      <home-text></home-text>
-    </div>
-    <div class="column">
-      <!--<tree-tabs></tree-tabs>-->
+    <div class="column is-2"></div>
+    <div class="column is-8">
       <div class="field">
-        <div class="disclaimer">
-          <p class="disclaimer-text">
-            * Projects under a certain value, relative to the largest project within each subsector, are not shown in this chart. To see all projects, please visit the <router-link to="/tables/projects">projects table</router-link>.
-          </p>
-        </div>
+        <p class="main-text">
+          Le RCPCA est le Plan National de Relèvement et de Consolidation de la Paix de la République Centrafricaine (RCA). Le RCPCA couvre la période 2017-2021. Le rapport de synthèse du RCPCA a été approuvé par le Gouvernement en octobre 2016 et présenté aux partenaires de la RCA le 17 novembre 2016 lors de la Conférence de Bruxelles. 
+          <br>
+          <br>
+          Ce site présente les données du suivi de l’aide extérieure pour l’année 2017 pour la RCA. Les données ont été collectées lors de l’exercice de cartographie de l’aide qui a eu lieu à fin 2017, en vue d’alimenter le rapport de la revue annuelle de la première année du RCPCA. Les données ont été récoltées par l’Unité de suivi de l’aide du Secrétariat du RCPCA. En savoir plus sur l'exercice de cartographie de l'aide.
+          <br>
+          <br>
+          L’arborescence de cette page permet d’explorer les projets de relèvement, consolidation de la paix et développement par piliers, composantes et secteurs du RCPCA. Elle n’inclut pas l’aide humanitaire d’urgence, qui fait l’objet du Plan de réponse humanitaire et dont les données sont accessibles sur la base FTS d’OCHA.
+        </p>
         <div>
-          <p id="support">Developed with the support of the World Bank and United Nations.</p>
+          <p id="support">Développé avec le support de la Banque mondiale.</p>
         </div>
       </div>
     </div>
+    <div class="column is-2"></div>
   </div>
 </template>
 
@@ -56,5 +58,13 @@
   	margin: 2%;
   	font-size: 11px;
   	color: gray;
+  }
+  .main-text {
+  	font-size: 16px;
+  	/*text-align: justify;*/
+  	line-height: 1.5;
+  	margin: 2%;
+  	margin-top: 2%;
+  	font-weight: 400;
   }
 </style>
