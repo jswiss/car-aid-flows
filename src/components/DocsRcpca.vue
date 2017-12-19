17f468601208bc5b9be0c146ae1f8a83e8231570
Joshua Swiss <swissjoshua@gmail.com>
2017-12-19 19:06:38 +0000

    added text and links for new documents page

--- src/components/DocsRcpca.vue
+++ src/components/DocsRcpca.vue
@@ -1,12 +1,33 @@
 <template>
-  <div class="container">
-    <div class="main-text">
-      Testing
+  <div id="topmost" class="columns is-mobile">
+    <div class="column is-2 buffer"></div>
+    <div class="column is-8">
+      <p class="is-2 title">Documents de référence du RCPCA</p>
+      <br>
+        <p class="text">
+          Le RCPCA est le Plan National de Relèvement et de Consolidation de la Paix de la
+          République Centrafricaine (RCA). Le RCPCA couvre la période 2017-2021. Les
+          principaux documents de référence du RCPCA sont listés ci-dessous et peuvent être
+          téléchargés.
+        </p>
+        <br>
+        <div class="link"><a href="https://s3.eu-central-1.amazonaws.com/car-downloads/RCPCA+final+A4_FR.pdf">Rapport de synthèse du RCPCA</a> <span>(octobre 2016)</span></div>
+        <div class="link"><a href="https://s3.eu-central-1.amazonaws.com/car-downloads/RCPCA_Rapport+technique.pdf">Rapport technique du RCPCA</a> <span>(octobre 2016)</span></div>
+        <div class="link"><a href="https://s3.eu-central-1.amazonaws.com/car-downloads/Strate%C3%ACgiedeCommunicationduRCPCA_FINAL.pdf">Stratégie de communication du RCPCA</a> <span>(novembre 2017)</span></div>
+        <div class="link"><a href="https://s3.eu-central-1.amazonaws.com/car-downloads/Revue+annuelle+RCPCA+2017_VOLUME+I+PARTIE+1.pdf">Revue annuelle du RCPCA an 1 – partie 1</a> <span>(novembre 2017)</span></div>
+        <div class="link"><a href="https://s3.eu-central-1.amazonaws.com/car-downloads/Revue+annuelle+RCPCA+2017_VOLUME+I+PARTIE+2.pdf">Revue annuelle du RCPCA an 1 – partie 2</a> <span>(novembre 2017)</span></div>
+        <div class="link"><a href="https://s3.eu-central-1.amazonaws.com/car-downloads/VOLUME+III+Rapport+Technique.pdf">Revue annuelle du RCPCA an 1 – annexe 1</a> <span>(novembre 2017)</span></div>
     </div>
+    <div class="column is-2"></div>
   </div>
 </template>
 
 <style scoped>
-
+  #topmost {
+  	margin-top: 20px;
+  }
+  .buffer {
+  	margin: 20px;
+  }
 </style>
 
